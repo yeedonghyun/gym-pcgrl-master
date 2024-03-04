f36fb3b7f70589fb29b676fe6c97780b796dc71d
dongHyun Lee <dledgus@naver.com>
2024-03-04 12:22:59 +0900

    clean code up

--- gym_pcgrl/envs/probs/__init__.py
+++ gym_pcgrl/envs/probs/__init__.py
@@ -1,20 +1,8 @@
-from gym_pcgrl.envs.probs.binary_prob import BinaryProblem
-from gym_pcgrl.envs.probs.ddave_prob import DDaveProblem
-from gym_pcgrl.envs.probs.mdungeon_prob import MDungeonProblem
-from gym_pcgrl.envs.probs.sokoban_prob import SokobanProblem
-from gym_pcgrl.envs.probs.zelda_prob import ZeldaProblem
-from gym_pcgrl.envs.probs.smb_prob import SMBProblem
 from gym_pcgrl.envs.probs.match3_prob import Match3Problem
 from gym_pcgrl.envs.probs.maze_prob import MazeProblem
 
 # all the problems should be defined here with its corresponding class
 PROBLEMS = {
-    "binary": BinaryProblem,
-    "ddave": DDaveProblem,
-    "mdungeon": MDungeonProblem,
-    "sokoban": SokobanProblem,
-    "zelda": ZeldaProblem,
-    "smb": SMBProblem,
     "match3": Match3Problem,
     "maze": MazeProblem
 
--- inference.py
+++ inference.py
@@ -1,39 +1,27 @@
-import numpy as np
-
 from stable_baselines import PPO2
 from utils import make_vec_envs
-from datetime import datetime
 from gym_pcgrl.envs.helper import save_image
 
-def inference(game, representation, model_path, output_file_path, **kwargs):
-    start_time = datetime.now().replace()
-
+def inference(game, representation, **kwargs):
     env_name = '{}-{}-v0'.format(game, representation)
     env = make_vec_envs(env_name, representation, None, 1, **kwargs)
-
+    model_path = 'models/{}/{}/model.pkl'.format(game, representation)
     model = PPO2.load(model_path)
     
     for i in range(kwargs.get('trials', 1)):
         obs = env.reset()
-        for j in range(1000) :
+        for _ in range(1000) :
             action, _ = model.predict(obs)
             obs, _, dones, info = env.step(action)
 
             if dones:        
-                path = output_file_path + str(i) + '_' + str(info[0]['crossroads'])
+                path = 'image/{}/'.format(game) + str(i) + '_' + str(info[0]['swap_potential'])
                 save_image(info[0]['terminal_observation'], path)
                 break
 
-    end_time = datetime.now().replace()
-    print("Start time  : ", start_time)
-    print("End time  : ", end_time)
-    print("Total inference time  : ", end_time - start_time)
-
 ################################## MAIN ########################################
-game = 'maze'
+game = 'match3'
 representation = 'wide'
-model_path = 'models/{}/{}/model.pkl'.format(game, representation)
-output_file_path = 'image/{}/'.format(game)
 kwargs = {
     'trials': 5,
     'verbose': False,
@@ -41,4 +29,4 @@ kwargs = {
 }
 
 if __name__ == '__main__':
-    inference(game, representation, model_path, output_file_path, **kwargs)
+    inference(game, representation, **kwargs)
